c0985fb0a676a7e70490b4a7260f513af3cda6e9
Swati Chitale <swati.chitale@shauryatechnosoft.com>
2022-09-02 11:05:27 +0530

    AddCompettionImages:repository and interface

--- GaneshFestival/Repository/CompetitionAsyncRepository.cs
+++ GaneshFestival/Repository/CompetitionAsyncRepository.cs
@@ -105,9 +105,12 @@ namespace GaneshFestival.Repository
                     {
                    
                         images.CompetitionId = id;
+                        //var query1 = @"Insert into tblCompetitionImages(CompetitionId,ImagePath,IsMainImage,IsImage,IsDeleted)
+                        //               values(@EventId,@ImagePath,@IsMainImage,@IsImage,0)";
                         var query1 = @"Insert into tblCompetitionImages(CompetitionId,ImagePath,IsMainImage,IsImage,IsDeleted)
-                                       values(@EventId,@ImagePath,@IsMainImage,@IsImage,0)";
+                                       values(@CompetitionId,@ImagePath,@IsMainImage,@IsImage,0)";
                         var res = await dbConnection.ExecuteAsync(query1, images);
+
                     }
                 }
                 return result;
